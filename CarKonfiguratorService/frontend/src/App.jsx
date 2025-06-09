bcd4bdbf7ae2f5a2e28f7b8be132900e3e70448e
EnesKuecuekakyuez <enes3008@gmail.com>
2025-06-09 14:35:44 +0200

    first commit

---
+++ CarKonfiguratorService/frontend/src/App.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [array, setArray] = useState([])
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:8080")
+    setArray(response.data.blogPost)
+  }
+
+
+  useEffect(() => {
+    fetchData();
+
+  }, [])
+  return (
+    <>
+     <div>
+      <h1>
+      test
+      </h1>
+
+      <ul>
+          {
+            array.map((blog,index) => (
+              <li key={index}
+              className='bg-sky-100 p-4 rounded-2xl transition-transform transform hover:scale-105'>
+                <p className='text-x1 font-semibold text-gray-800'>
+                  {blog.title}
+                </p>
+                <p className='text-sm text-gray-100'>
+                  {blog.content}
+                </p>
+              </li>
+            ))
+          }
+      </ul>
+     </div>
+    </>
+  )
+}
+
+export default App
